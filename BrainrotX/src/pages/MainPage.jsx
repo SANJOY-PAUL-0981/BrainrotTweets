a1ab4ef479290a2cdf29a47339784dbf5b01555f
SANJOY <paulsanjoy2923@gmail.com>
2024-11-01 15:59:16 +0530

    gemini API added and setup finished

--- BrainrotX/src/pages/MainPage.jsx
+++ BrainrotX/src/pages/MainPage.jsx
@@ -1,8 +1,26 @@
+import axios from "axios";
+import { useState } from "react";
+
 function MainPage(){
+    async function generateAnswer(){
+        console.log("loading...")
+
+        const response = await axios({
+            url: "https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash-latest:generateContent?key=AIzaSyBk5ceq9XMw1sWQn3HtAJGaNNAjCGfHQ1Y",
+            method: "post",
+            data: {
+                contents:[
+                    { parts: [{ text: "Explain how AI works" }] }
+                ]
+            }
+        })
+        console.log(response['data']['candidates'][0]['content']['parts'][0]['text']);
+    }
+
     return(
-        <div className="bg-green-400">
-            hii
-        </div>
+        <button onClick={generateAnswer} className="bg-purple-500 p-2 text-center">
+            Response
+        </button>
     )
 }
 export default MainPage;

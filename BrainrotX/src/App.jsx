1cd4fb8a6f8236523564264bde6041e3166052fd
SANJOY <paulsanjoy2923@gmail.com>
2024-11-01 15:11:50 +0530

    file setup done

--- BrainrotX/src/App.jsx
+++ BrainrotX/src/App.jsx
@@ -1,16 +1,12 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import MainPage from "./pages/MainPage.jsx"
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-    <div className='bg-yellow-400'>
-      hi
-    </div>
+      <MainPage />
     </>
   )
 }
---
+++ BrainrotX/src/pages/MainPage.jsx
@@ -0,0 +1,8 @@
+function MainPage(){
+    return(
+        <div className="bg-green-400">
+            hii
+        </div>
+    )
+}
+export default MainPage;

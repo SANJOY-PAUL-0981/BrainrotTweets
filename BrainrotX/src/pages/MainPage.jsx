fc65e36a90f391fd85e4e755127f09188cb56e63
SANJOY <paulsanjoy2923@gmail.com>
2024-11-01 17:00:44 +0530

    backend ready

--- BrainrotX/src/pages/MainPage.jsx
+++ BrainrotX/src/pages/MainPage.jsx
@@ -1,26 +1,40 @@
 import axios from "axios";
+import { div } from "framer-motion/client";
 import { useState } from "react";
 
-function MainPage(){
-    async function generateAnswer(){
+function MainPage() {
+    const [question, setQuestion] = useState("");
+
+
+    async function generateAnswer() {
         console.log("loading...")
 
         const response = await axios({
             url: "https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash-latest:generateContent?key=AIzaSyBk5ceq9XMw1sWQn3HtAJGaNNAjCGfHQ1Y",
             method: "post",
             data: {
-                contents:[
-                    { parts: [{ text: "Explain how AI works" }] }
+                contents: [
+                    { parts: [{ text: question }] }
                 ]
             }
         })
         console.log(response['data']['candidates'][0]['content']['parts'][0]['text']);
     }
 
-    return(
-        <button onClick={generateAnswer} className="bg-purple-500 p-2 text-center">
-            Response
-        </button>
+    return (
+        <div>
+            <div className="flex flex-col gap-3 justify-center items-center">
+                <textarea
+                value={question} onChange={(e) => setQuestion(e.target.value)}
+                cols="30"
+                row="10" 
+                className="border-2 border-black">
+                </textarea>
+                <button onClick={generateAnswer} className="bg-purple-500 p-2 text-center">
+                    Response
+                </button>
+            </div>
+        </div>
     )
 }
 export default MainPage;
